aea669a4d2360b4602c7aa532343852963e95c68
Ryan Van Voorhis <rjvanvoorhis@crimson.ua.edu>
2019-03-14 07:34:25 -0400

    Add progress spinner when uploading image

--- config/config.js
+++ config/config.js
@@ -16,7 +16,14 @@ module.exports = {
             {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader']
-             }
+            },
+            {
+                test: /\.(png|jp(e*)g|svg|gif)$/,
+                use: [{
+                    loader: 'url-loader',
+                    options: {limit: 8000, name: 'images/[hash]-[name].[ext]'}
+                }]
+            }
         ]
     },
     plugins: [new HtmlWebpackPlugin({
--- src/ProgressCard/ProgressCard.jsx
+++ src/ProgressCard/ProgressCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {ProgressBar} from 'react-bootstrap'
-
+import loadingGif from '../images/loading.gif'
 
   class ProgressCard extends React.Component {
 
@@ -15,11 +15,12 @@ import {ProgressBar} from 'react-bootstrap'
     render() {
       let progress_style = this.get_level(parseInt(this.props.current_progress))
       return (
-        <div className="card h-100">
+        <div className="card h-100" style={{backgroundColor: "black"}}>
           <img className="card-img-top" src={this.props.current_image} alt="Current frame"/>
           <div className="card-body">
             <ProgressBar active bsStyle={progress_style} now={this.props.current_progress} label={`${this.props.current_progress} % complete`}/>
           </div>
+	      <img className="card-img-bottom" src={loadingGif} alt="Card image bottom"/>
         </div>
       )
     }
